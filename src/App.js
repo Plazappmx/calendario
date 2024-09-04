c10e5af520c7398537693bdb0b5b625a469b154a
Plazappmx <contacto@plazapp.mx>
2024-09-04 14:23:59 -0500

    se agrego boton de regreso a plazapp

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 // src/App.js
 import React from 'react';
 import AppointmentCalendar from './Calendar';
+import {Button, ThemeProvider, createTheme, CssBaseline, Container, Typography } from '@mui/material';
+
+const handleRedirect = () => {
+  window.location.href = 'https://www.plazapp.mx';
+};
 
 function App() {
   return (
     <div className="App">
+       <Button
+        variant="contained"
+        color="secondary"
+        onClick={handleRedirect}
+        sx={{ marginTop: 2 }}
+      >
+        Regresar a Plaza App
+      </Button>
       <AppointmentCalendar />
     </div>
   );
